33903c24a28681645cc856d4cad854737d8d7c83
MOKTAR ALHASAPI <moktaralhasapi@gmail.com>
2026-09-12 16:11:19 +0100

    Add visual Studium activity toolbox

--- mods/edu_core/init.lua
+++ mods/edu_core/init.lua
@@ -26,3 +26,45 @@ end
 function edu.escape(text)
 	return minetest.formspec_escape(tostring(text))
 end
+
+local function open_toolbox(name)
+	minetest.show_formspec(name, "edu_core:toolbox", table.concat({
+		"formspec_version[4]", "size[8,5]",
+		"label[2.2,0.35;Studium]",
+		"item_image_button[0.5,1;2,2;edu_number_blocks:board;arithmetic;]",
+		"item_image_button[3,1;2,2;edu_english_blocks:board;english;]",
+		"item_image_button[5.5,1;2,2;edu_logic_blocks:board;logic;]",
+		"label[0.65,3.2;Arithmetic]", "label[3.25,3.2;English]", "label[5.85,3.2;Logic]",
+		"button_exit[2.5,4;3,0.8;close;Done]",
+	}, ""))
+end
+
+minetest.register_node("edu_core:toolbox", {
+	description = "Studium Toolbox",
+	inventory_image = "edu_core_toolbox.png",
+	tiles = {"edu_core_toolbox.png"},
+	groups = {choppy = 2, oddly_breakable_by_hand = 2},
+	on_rightclick = function(_pos, _node, player)
+		open_toolbox(player:get_player_name())
+	end,
+})
+
+minetest.register_chatcommand("edu_menu", {
+	description = "Open the Studium activity toolbox",
+	func = function(name)
+		open_toolbox(name)
+		return true
+	end,
+})
+
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+	if formname ~= "edu_core:toolbox" then return false end
+	local name = player:get_player_name()
+	local command = fields.arithmetic and "edu_blocks"
+		or fields.english and "edu_english"
+		or fields.logic and "edu_logic"
+	if command and minetest.registered_chatcommands[command] then
+		minetest.registered_chatcommands[command].func(name)
+	end
+	return true
+end)
